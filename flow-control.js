5320f38a1c5ecd46348b5f0a570066a6dbc62423
Elizabeth T Hallett <ethallett@gmail.com>
2019-11-25 06:02:45 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -22,7 +22,7 @@ function ageChecker(age) {
 
 }
 
-function ternaryTeenager(age){}
+function ternaryTeenager(age){
  (age > 12 && age < 20) ?
  return("You are a teenager") :
  return("You are not a teenager")
