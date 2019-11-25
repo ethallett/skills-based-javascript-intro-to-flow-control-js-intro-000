72674e404f58ee1abc63026cb1d6422cc9bdfa70
Elizabeth T Hallett <ethallett@gmail.com>
2019-11-25 07:12:37 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -29,7 +29,7 @@ function ternaryTeenager(age){
 
 function switchAge(age) {
   switch (age) {
-   case (age > 12 && age < 20) :
+   case > 12 && < 20 :
    console.log("You are a teenager");
    break;
    default:
