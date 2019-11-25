ad0e1f433722fba7577844126554818acae852a3
Elizabeth T Hallett <ethallett@gmail.com>
2019-11-25 05:42:40 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -18,7 +18,7 @@ function ageChecker(age) {
     return("You are a teenager!");
   } else if(age <= 12){
     return("You are a kid");
-  } else (age >= 20){return("You are a grownup")}
+  } else {return("You are a grownup")}
 
 }
 
