7647908aae686c2fd4d50498581fb361f304a7ec
Elizabeth T Hallett <ethallett@gmail.com>
2019-11-25 05:30:13 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,26 +1,15 @@
-function basicTeenager(age){
+function basicTeenager(age) {
   if(age > 12 && age < 20){
     return("You are a teenager!");
-}
+  }
 
 }
 
-function teenager(age){
-  if(age > 12 && age < 20){
-    return("You are a teenager!")
-  } else {
-    return("You are not a teenager")}
-
-
+function teenager(age) {
 
 }
 
 function ageChecker(age) {
-
-  }
-
-
-
 
 }
 
@@ -29,33 +18,5 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
-  switch(age) {
-    case "13":
-      console.log("You are a teenager");
-      break;
-    case "14":
-      console.log("You are a teenager");
-      break;
-    case "15":
-      console.log("You are a teenager");
-      break;
-    case "16":
-      console.log("You are a teenager");
-      break;
-    case "17":
-      console.log("You are a teenager");
-      break;
-    case "18":
-      console.log("You are a teenager");
-      break;
-    case "19":
-      console.log("You are a teenager");
-      break;
-    default:
-      console.log("You have an age");
-    }
-
-
-
 
 }
